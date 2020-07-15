aded7ed95048eb43597c7c507447fd5cb589102b
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2020-07-15 00:22:15 -0300

    print date of backend in frontend

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,39 @@
+<?php
+
+  /*Get links from database*/
+  $sql = "SELECT * FROM site_information";
+  $query = mysqli_query($database , $sql);
+
+  if($query && mysqli_num_rows($query) == 1){
+    $links = mysqli_fetch_assoc($query);
+
+    $facebook_link = $links['facebook_link'];
+    $instagram_link = $links['instagram_link'];
+    $twitter_link = $links['twitter_link'];
+    $whatsapp_number = $links['whatsapp_number'];
+    $email = $links['email'];
+    $phone_number = $links['phone_number'];
+
+  }
+
+?>
+
+
 <footer>
-  <span>John Smith 2020</span>
+  <span>John Smith <?php echo date('yy'); ?></span>
 
   <div class="row">
-    <a href="#" class="col-md-3">
+    <a href="<?= $facebook_link ?>" class="col-md-3">
       <i class="facebook icon"></i>Facebook
     </a>
-    <a href="#" class="col-md-3">
+    <a href="https://api.whatsapp.com/send?phone=<?= $whatsapp_number ?>&text=Hello%20John%20Smith" class="col-md-3">
       <i class="whatsapp icon"></i>WhatsApp
     </a>
-    <a href="#" class="col-md-3">
-      <i class="youtube icon"></i>YouTube
+    <a href="<?= $instagram_link ?>" class="col-md-3">
+      <i class="instagram icon"></i>instagram
     </a>
-    <a href="#" class="col-md-3">
-      <i class="linkedin in icon"></i>Linkedin
+    <a href="mailto:<?= $email ?>" class="col-md-3">
+      <i class="paper plane icon"></i>Email
     </a>
   </div>
 
