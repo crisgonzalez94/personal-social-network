cbac59aeafb352b4b5b61ffb9f111b4bfdafdac4
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2020-07-15 01:03:23 -0300

    print date of backend in pages/home.php

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,17 @@
 <?php
 
   /*Get Biography of database*/
+  $sql = "SELECT * FROM home";
+  $query = mysqli_query($database , $sql);
 
+  if($query && mysqli_num_rows($query) == 1){
+    $home = mysqli_fetch_assoc($query);
+
+    $title = $home['title'];
+    $biography = $home['biography'];
+    $photo = $home['photo'];
+
+  }
 
  ?>
 
@@ -11,14 +21,11 @@
 
   <div class="row no-gutters bg-light position-relative">
     <div class="col-md-6 mb-md-0 p-md-4">
-      <img src="assets/images/profile-autor-image/profile-autor-image.jpg" class="w-100 img-home" alt="John Smith Image Profile" >
+      <img src="assets/images/profile-autor-image/<?= $photo  ?>" class="w-100 img-home" alt="John Smith Image Profile" >
     </div>
     <div class="col-md-6 position-static p-4 pl-md-0">
-      <h4 class="mt-0">Desingning is my passion</h4>
-      <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae sem in diam ultrices aliquet. Praesent purus odio, lobortis vel nisl ut, fermentum facilisis ex. Nam in finibus magna, et fringilla metus. Morbi eu efficitur erat, in tincidunt quam. Donec sit amet massa diam. Sed ac sem vestibulum, eleifend urna sed, mollis ligula. Donec leo eros, porttitor eu commodo nec, aliquet vitae diam. </p>
-      <p>Vestibulum vitae erat dui. Ut sit amet pretium diam. Nulla at purus nunc. Duis erat dui, ullamcorper non mattis nec, molestie id diam. Maecenas mattis diam eu tortor auctor, id dapibus metus eleifend. Duis eu lectus hendrerit, egestas velit eget, facilisis neque. Nulla justo felis, malesuada ac dolor sit amet, condimentum finibus quam. Maecenas suscipit ex velit. Donec volutpat metus turpis, non fringilla massa suscipit eget. Morbi a nisl et nisl lobortis vestibulum ut eu magna. Cras non tempor enim. Nunc vestibulum magna ut mi egestas sollicitudin. Proin non sapien in mauris lacinia condimentum eget nec neque. Morbi faucibus, turpis nec elementum luctus, justo libero semper ex, vitae porttitor nunc quam non arcu. </p>
-      <p>Aliquam elementum finibus tempor. Cras auctor sed justo et efficitur. Quisque non posuere lectus. Donec ullamcorper nulla risus, eget lobortis odio tincidunt quis. Sed non tellus fringilla, finibus metus ac, eleifend arcu. Pellentesque vel tempus augue. Sed ac sem at neque convallis elementum. Nulla facilisi. Praesent id aliquam elit, et auctor elit. Ut dui urna, porta at neque quis, auctor pretium nunc. </p>
+      <h4 class="mt-0"><?= $title  ?></h4>
+      <p><<?= $biography  ?>/p>
     </div>
   </div>
 
