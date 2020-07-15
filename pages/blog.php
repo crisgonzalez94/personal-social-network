c889b143cbe102d81ee022071882087923301a3d
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2020-07-15 01:29:01 -0300

    print date of backend in pages/blog.php

--- pages/blog.php
+++ pages/blog.php
@@ -1,66 +1,47 @@
+<?php
+
+  /*Get events from database*/
+  $sql = "SELECT * FROM posts";
+  $posts = mysqli_query($database , $sql);
+
+?>
+
+
 <div class="container">
   <h1>Blog</h1>
   <hr>
 
-  <div class="card mb-3">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img src="assets/images/audios/1.jpg" class="card-img" alt="El nuevo album">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h3 class="h1">Card title</h3>
-
-
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-
-          <button type="button" class="btn btn-dark">Show more</button>
-
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php while($post = mysqli_fetch_assoc($posts)): ?>
+  <?php
+    $id = $post['id'];
+    $title = $post['title'];
+    $short_description = $post['short_description'];
+    $body = $post['body'];
+    $picture = $post['picture'];
+    $date_post = $post['date_post'];
+  ?>
 
   <div class="card mb-3">
     <div class="row no-gutters">
       <div class="col-md-4">
-        <img src="assets/images/audios/1.jpg" class="card-img" alt="El nuevo album">
+        <img src="assets/images/posts/<?= $picture ?>" class="card-img" alt="<?= $short_description ?>">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h3 class="h1">Card title</h3>
+          <h3 class="h1"><?= $short_description ?></h3>
 
 
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><?= $body ?></p>
 
           <button type="button" class="btn btn-dark">Show more</button>
 
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <p class="card-text"><small class="text-muted"><?= $date_post ?></small></p>
         </div>
       </div>
     </div>
   </div>
+  <?php endwhile; ?>
 
-  <div class="card mb-3">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img src="assets/images/audios/1.jpg" class="card-img" alt="El nuevo album">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h3 class="h1">Card title</h3>
-
-
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-
-          <button type="button" class="btn btn-dark">Show more</button>
-
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
 
