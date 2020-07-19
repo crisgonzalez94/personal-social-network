bf1520cd6dbfe9ed13bb89d40aedc353efbb5e44
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2020-07-19 13:16:39 -0300

    Ad meta viewport , todavia no lo habia agradado :( :(

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,9 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>John Smith</title>
 
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
